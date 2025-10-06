785ac90f8b3a240223436a23ca4516aca12f4eb0
vikas2-alt <enjamurivikas@gmail.com>
2025-10-06 15:12:55 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,9 @@
+public class Test {
+
+        public static void main(String[] args) {
+          system.out.println("**************************************************");
+          system.out.println("vikas alt2 test-java is working");
+          system.out.println("**************************************************");
+        }
+
+}
