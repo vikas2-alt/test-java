2d45c34212ca052232909fed13415b0400be543c
Vikas <enjamurivikas@gmail.com>
2025-10-07 16:30:01 +0530

    test done

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+        public static void main(String[] args) {
+          System.out.println("**************************************************");
+          System.out.println("vikas alt2 test-java is working");
+          System.out.println("**************************************************");
+        }
+
+}
--- Test1.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-
-        public static void main(String[] args) {
-          system.out.println("**************************************************");
-          system.out.println("vikas alt2 test-java is working");
-          system.out.println("**************************************************");
-        }
-
-}
